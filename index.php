cfba1c7c7a638dd34612dfdbb47af1ac47618811
malko <msa>
2022-01-09 15:12:23 +0100

    v1.1

--- index.php
+++ index.php
@@ -34,28 +34,38 @@ $conn = new mysqli("czaplinek.home.pl:3306", "00018732_kw", "Kajet@nW0j25", "000
             background-color: white;
         }
 
-        }
-
     </style>
 </head>
 <body>
-<div style="margin: 0 auto; width: 1410px;margin-top: 150px;">
+
+<div style="margin: 0 auto; height : 40px; width: 194px;  padding-top: 150px;">
+    <form method="post">
+        <input style="width: 95px" type="text" id="tytul" name="tytul">
+        <input style="width: 95px" type="submit" <?php
+        error_reporting(null);
+        ?>
+               value="Szukaj">
+    </form>
+</div>
+
+<div style="margin: 0 auto; width: 1410px;margin-top: 10px;">
+
     <div style="background-color: white; float: left; margin-right: 6px">
-        <table style="width:702px;">
+        <table style="width:700px;">
             <tr class="naglowki">
-                <th>
+                <th style="width:44px">
                     idGry
                 </th>
-                <th>
+                <th style="width:232px">
                     nazwa
                 </th>
-                <th>
+                <th style="width:45px">
                     rok
                 </th>
-                <th>
+                <th style="width:232px">
                     wydawca
                 </th>
-                <th>
+                <th style="width:148px">
                     gatunek
                 </th>
             </tr>
@@ -64,11 +74,17 @@ $conn = new mysqli("czaplinek.home.pl:3306", "00018732_kw", "Kajet@nW0j25", "000
         <div style="height:500px;width:700px;border:1px solid black; overflow: scroll">
             <table style="width:100%">
                 <?php
-                $sql = "SELECT idGry, nazwa, rokWydania, wydawca, gatunek FROM Gry;";
+                error_reporting(null);
+                $tytul = '%'.$_POST["tytul"].'%';
+                if($tytul=="%%"){
+                    $sql = "SELECT idGry, nazwa, rokWydania, wydawca, gatunek FROM Gry;";
+                }else{
+                    $sql = "SELECT idGry, nazwa, rokWydania, wydawca, gatunek FROM Gry WHERE nazwa LIKE '$tytul';";
+                }
                 $result = $conn->query($sql);
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
-                        echo sprintf("<tr onclick=\"location.href='Stronka.php?gra=%s'\"><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", $row["idGry"], $row["idGry"], $row["nazwa"], $row["rokWydania"], $row["wydawca"], $row["gatunek"]);
+                        echo sprintf("<tr onclick=\"location.href='index.php?gra=%s'\"><td style=\"width:45px\">%s</td><td style=\"width:235px\">%s</td><td style=\"width:45px\">%s</td><td style=\"width:243px\">%s</td><td style=\"width:132px\">%s</td></tr>", $row["idGry"], $row["idGry"], $row["nazwa"], $row["rokWydania"], $row["wydawca"], $row["gatunek"]);
                     }
                 } else {
                     echo "0 results";
@@ -79,18 +95,18 @@ $conn = new mysqli("czaplinek.home.pl:3306", "00018732_kw", "Kajet@nW0j25", "000
     </div>
 
     <div style="background-color: white; float : left">
-        <table style="width:702px">
+        <table style="width:700px">
             <tr class="naglowki">
-                <th>
+                <th style="width: 120px">
                     idEgzemplarza
                 </th>
-                <th>
+                <th style="width: 210px">
                     stan
                 </th>
-                <th>
+                <th style="width: 100px">
                     cena
                 </th>
-                <th>
+                <th style="width: auto">
                     placowka
                 </th>
             </tr>
@@ -106,7 +122,22 @@ $conn = new mysqli("czaplinek.home.pl:3306", "00018732_kw", "Kajet@nW0j25", "000
                     $result = $conn->query($sql);
                     if ($result->num_rows > 0) {
                         while ($row = $result->fetch_assoc()) {
-                            echo sprintf("<tr onclick=\"location.href='Stronka.php?gra=%s&egzemplarz=%s'\"><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", $gra, $row["idEgzemplarza"], $row["idEgzemplarza"], $row["stan"], $row["cena"], $row["miasto"]);
+                            $stan = "fatalny";
+                            switch ($row["stan"]) {
+                                case 1:
+                                    $stan = "zły";
+                                    break;
+                                case 2:
+                                    $stan = "używany";
+                                    break;
+                                case 3:
+                                    $stan = "dobry";
+                                    break;
+                                case 4:
+                                    $stan = "wzorowy";
+                                    break;
+                            }
+                            echo sprintf("<tr onclick=\"location.href='index.php?gra=%s&egzemplarz=%s'\"><td style='width: 120px'>%s</td><td style='width: 210px'>%s</td><td style='width: 100px'>%szł</td><td style='width: auto'>%s</td></tr>", $gra, $row["idEgzemplarza"], $row["idEgzemplarza"], $stan, $row["cena"], $row["miasto"]);
                         }
                     }
                 }
@@ -174,7 +205,7 @@ $conn = new mysqli("czaplinek.home.pl:3306", "00018732_kw", "Kajet@nW0j25", "000
     ?>
 </div>
 
-<div style="width: 350px;margin: 0 auto ; padding-top: 20px; clear: left">
+<div style="width: 450px;margin: 0 auto ; padding-top: 20px; clear: left">
     <?php
     if ($odb != "") {
         echo "<marquee><span style='font-family: \"Comic Sans MS\";color: white;text-align: center ;font-size: 23px; '> <br> IDTransakcji: " . $id . ". Podaj ten numer sprzedawcy.</span></marquee>";
